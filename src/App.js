27fb24439770c31b7f55ed2213a054326c1460f8
Imikan10 <basseyimikan10@gmail.com>
2024-04-30 10:02:08 +0100

    list of cities for tours

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+import { Places } from './data'
+
+const App = () => {
+  // const [showMore, setShowMore] = useState(false)
+  const [location, setLocation] = useState(Places)
+
+  const toggleButton = (id) => {
+    setLocation(
+      location.map((city) =>
+        city.id === id ? { ...city, showMore: !city.showMore } : city
+      )
+    )
+  }
+
+  const removeCity = (id) => {
+    let newCity = location.filter((city) => city.id !== id)
+    setLocation(newCity)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <main>
+        <section>
+          <div className='title'>
+            <h2>our tours</h2>
+            <div className='title-underline'></div>
+          </div>
+
+          <div className='tours'>
+            {location.map((city) => {
+              return (
+                <>
+                  <article key={city.id} className='single-tour'>
+                    <img className='image' src={city.img} alt='' />
+                    <div className='tour-info'>
+                      <span className='tour-price'>{city.price}</span>
+                      <h5>{city.place}</h5>
+                      <p>
+                        {city.about}
+                        {city.showMore && <> {city.more} </>}
+                        {!city.showMore && '...'}{' '}
+                        <button
+                          className='more'
+                          onClick={() => toggleButton(city.id)}
+                        >
+                          {city.showMore ? 'read less' : 'read more'}
+                        </button>
+                      </p>
+                      <button
+                        className='delete-btn'
+                        onClick={() => removeCity(city.id)}
+                      >
+                        Not Interested
+                      </button>
+                    </div>
+                  </article>
+                </>
+              )
+            })}
+          </div>
+        </section>
+      </main>
+    </>
+  )
 }
 
-export default App;
+export default App
